4861aef2cea43b5486813f17b48692783ff7a31a
shinixsensei <crezoxplays@gmail.com>
2020-12-11 19:59:40 +0100

    init

---
+++ src/main/java/Lib/Game.java
@@ -0,0 +1,30 @@
+package Lib;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+
+public class Game {
+
+    public static boolean checkAvailable() throws IOException {
+        String findProcess = "Cyberpunk2077.exe";
+        String filenameFilter = "/nh /fi \"Imagename eq "+findProcess+"\"";
+        String tasksCmd = System.getenv("windir") +"/system32/tasklist.exe "+filenameFilter;
+
+        Process p = Runtime.getRuntime().exec(tasksCmd);
+        BufferedReader input = new BufferedReader(new InputStreamReader(p.getInputStream()));
+
+        ArrayList<String> procs = new ArrayList<String>();
+        String line = null;
+        while ((line = input.readLine()) != null)
+            procs.add(line);
+
+        input.close();
+
+        Boolean processFound = procs.stream().filter(row -> row.indexOf(findProcess) > -1).count() > 0;
+
+        return processFound;
+    }
+
+}
---
+++ src/main/java/Main/Main.java
@@ -0,0 +1,52 @@
+package Main;
+
+import Lib.Game;
+import club.minnced.discord.rpc.DiscordEventHandlers;
+import club.minnced.discord.rpc.DiscordRPC;
+import club.minnced.discord.rpc.DiscordRichPresence;
+
+import java.io.IOException;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+
+        boolean processFound = Game.checkAvailable();
+
+        if(processFound) {
+
+            DiscordRPC lib = DiscordRPC.INSTANCE;
+            String applicationId = "787025364677296148";
+            String steamId = "1091500";
+            DiscordEventHandlers handlers = new DiscordEventHandlers();
+            handlers.ready = (user) -> System.out.println("Ready!");
+            lib.Discord_Initialize(applicationId, handlers, true, steamId);
+            DiscordRichPresence presence = new DiscordRichPresence();
+            presence.startTimestamp = System.currentTimeMillis() / 1000; // epoch second
+            presence.details = "in Night City";
+            presence.largeImageKey = "cp";
+            lib.Discord_UpdatePresence(presence);
+            // in a worker thread
+            new Thread(() -> {
+                while (!Thread.currentThread().isInterrupted()) {
+                    lib.Discord_RunCallbacks();
+                    try {
+                        Thread.sleep(2000);
+                    } catch (InterruptedException ignored) {}
+
+                    try {
+                        if(!Game.checkAvailable()) {
+                            System.out.println("Closed Game. Disconnecting.");
+                            System.exit(0);
+                        }
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                }
+            }, "RPC-Callback-Handler").start();
+
+        }else {
+            System.out.println("Process not found. Please start Cyberpunk 2077 and try again!");
+        }
+    }
+}
